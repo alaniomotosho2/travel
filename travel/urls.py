dc09d8b87e0cd61b4ff5e5cba6fbc8a5658d4894
alaniomotosho2 <alaniomotosho2@gmail.com>
2018-02-01 11:24:11 +0530

    ffourth

--- travel/urls.py
+++ travel/urls.py
@@ -13,6 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib.sitemaps.views import sitemap
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 
 from django.conf.urls import include,url
